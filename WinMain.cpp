cdf780253df5f98770f2438707e8f3a407f2daff
Shehbaz Khan <shehbaz.kh26@gmail.com>
2025-01-03 16:05:31 -0600

    finish t3

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,13 +1,34 @@
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+
+LRESULT CALLBACK WndProc(HWND hWnd,
+												 UINT msg,
+												 WPARAM wParam,
+												 LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+		default:
+			return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance,
+										 HINSTANCE hPrevInstance,
+										 LPSTR lpCmdLine,
+										 int nCmdShow)
 {
 	const auto pClassName = L"HelloDirectX";
 	// register window class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -20,14 +41,31 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 	RegisterClassEx(&wc);
 
-	HWND hWnd = CreateWindowEx(
-		0, pClassName, L"HelloDirectX",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480, nullptr, nullptr, hInstance, nullptr
+	HWND hWnd = CreateWindowEx(0, pClassName, L"HelloDirectX",
+														 WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+														 200, 200, 640, 480, nullptr, nullptr,
+														 hInstance, nullptr
 	);
 
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
+	
+	// message loop
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 
 	return 0;
 }
